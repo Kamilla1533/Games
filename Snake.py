8cba4d3c61c7347be6c2c7c6416d1068e99a518b
Kamilla <155493339+Kamilla1533@users.noreply.github.com>
2024-01-16 05:04:13 +0300

    Add files via upload

---
+++ Snake.py
@@ -0,0 +1,103 @@
+import pygame
+from pygame.locals import *
+import time
+
+class Snake:
+    def __init__(self, parent_screen, black_color, green_color, pos_x, pos_y):
+        self.black = black_color
+        self.green = green_color
+        self.parent_screen = parent_screen
+        self.snake_size = 35
+        self.snake_speed = 10
+        self.snake_pos_x = pos_x / 2
+        self.snake_pos_y = pos_y / 2
+        self.direction = "down"
+
+    def draw(self):
+        self.outer = pygame.draw.rect(self.parent_screen, self.black, (self.snake_pos_x, self.snake_pos_y, self.snake_size, self.snake_size))
+        self.inner = pygame.draw.rect(self.parent_screen, self.green, (self.snake_pos_x + 1, self.snake_pos_y + 1, self.snake_size - 2, self.snake_size - 2))
+
+
+    def left_move(self):
+        self.snake_pos_x -= self.snake_size
+        self.draw()
+
+    def right_move(self):
+        self.snake_pos_x += self.snake_size
+        #self.draw()
+
+    def up_move(self):
+        self.snake_pos_y -= self.snake_size
+        #self.draw()
+
+    def down_move(self):
+        self.snake_pos_y += self.snake_size
+        #self.draw()
+
+class MainGame:
+    def __init__(self):
+        # запуск игры
+        pygame.init()
+
+        # размер окна
+        self.SC_WIDTH = 720
+        self.SC_HEIGHT = 720
+
+        # Цвета
+        self.black = (51, 44, 57)
+        self.green = (96, 158, 162)
+        self.red = (201, 44, 109)
+        self.white = (240, 238, 237)
+
+        self.screen = pygame.display.set_mode((self.SC_WIDTH, self.SC_HEIGHT))
+        title = pygame.display.set_caption("Snake")
+        self.screen.fill(self.black)
+
+        self.snake = Snake(self.screen, self.black, self.green, self.SC_WIDTH, self.SC_HEIGHT)
+        self.snake.draw()
+
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+                    if event.key == K_UP:
+                        self.snake.up_move()
+                    elif event.key == K_DOWN:
+                        self.snake.down_move()
+                    elif event.key == K_LEFT:
+                        self.snake.left_move()
+                    elif event.key == K_RIGHT:
+                        self.snake.right_move()
+
+                elif event.type == QUIT:
+                    running = False
+            
+
+            pygame.display.update()
+
+        pygame.quit()
+
+if __name__ == '__main__':
+    Game = MainGame()
+    Game.run()
+
+
+'''
+def walk():
+    snake_pos_x = int(SC_WIDTH / 2)
+    snake_pos_y = int(SC_HEIGHT / 2)
+    direction = "down"
+    if direction == "left":
+        snake_pos_x -= snake_size
+    elif direction == "right":
+        snake_pos_x += snake_size
+    elif direction == "up":
+        snake_pos_y -= snake_size
+    elif direction == "down":
+        snake_pos_y += snake_size
+'''
+
+
