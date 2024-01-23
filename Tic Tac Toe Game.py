57bf1719855848cd7778c5cb70ca6513467caef0
Kamilla <155493339+Kamilla1533@users.noreply.github.com>
2024-01-23 19:00:30 +0300

    Update Tic Tac Toe Game.py

--- Tic Tac Toe Game.py
+++ Tic Tac Toe Game.py
@@ -18,6 +18,13 @@ CELL_HEIGHT = 200
 class Lattice:
     def __init__(self, parent_screen):
         self.parent_screen = parent_screen
+        self.board = [[1, 2, 3],
+                      [4, 5, 6],
+                      [7, 8, 9]]
+
+        self.graph_board = [[[None, None], [None, None], [None, None]],
+                            [[None, None], [None, None], [None, None]],
+                            [[None, None],[None, None], [None, None]]]
 
     def draw(self):
         pygame.draw.rect(self.parent_screen, LIGHT_BLUE_L, (0, 515, 610, 5))
@@ -27,46 +34,41 @@ class Lattice:
         pygame.draw.rect(self.parent_screen, LIGHT_BLUE_L, (405, 110, 5, 610))
 
 
-
 class X_player:
-    def __init__(self, parent_screen, pos_x, pos_y):
-        self.pos_x = pos_x
-        self.pos_y = pos_y
+    def __init__(self, parent_screen):
+        #self.pos_x = pos_x
+        #self.pos_y = pos_y
         self.parent_screen = parent_screen
         self.x_image = pygame.image.load("x_player.png").convert()
-
-    def draw_x(self):
-        self.parent_screen.blit(self.x_image, (self.pos_x, self.pos_y))
-        pygame.display.flip()
 
 
 class O_player:
     def __init__(self, parent_screen):
+        #self.pos_x = pos_x
+        #self.pos_y = pos_y
         self.parent_screen = parent_screen
-        self.o_image = pygame.image.load("o_player.png")
+        self.o_image = pygame.image.load("o_player.png").convert()
 
     def draw_o(self):
-        self.parent_screen.blit(self.o_image, (205, 110))
+        self.parent_screen.blit(self.o_image, (None, None))
         pygame.display.flip()
-        pygame.draw.circle(self.parent_screen, WHITE_O, (305,210), 70, 5)
+
 
 class MainGame:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        title = pygame.display.set_caption("Tic Tac Toe")
+        title = pygame.display.set_caption("Tic Tac Toe Game")
         self.screen.fill(BLUE_BG)
 
-        #self.x_player = X_player(self.screen)
-        #self.x_player.draw_x()
+        self.x_player = X_player(self.screen)
 
-        #self.o_player = O_player(self.screen)
-        #self.o_player.draw_o()
+        self.o_player = O_player(self.screen)
 
         self.lattice = Lattice(self.screen)
         self.lattice.draw()
 
-        font = pygame.font.SysFont("robotho", 40)
+        font = pygame.font.SysFont("robotho", 45)
         score = font.render("SCORE", True, WHITE_O)
         self.screen.blit(score, (250, 10))
 
@@ -77,30 +79,29 @@ class MainGame:
         #score = font.render("SCORE",True, WHITE_O)
         #self.screen.blit(score, (250, 10))
 
+    def draw_lat(self):
+        for i in range(3):
+            for j in range(3):
+                if self.lattice.board[i][j] == "X":
+                    self.lattice.graph_board[i][j][0] = self.x_player.x_image
+                    self.lattice.graph_board[i][j][1] = self.x_player.x_image.get_rect(center =(j*300+150, i*300+150))
+                elif self.lattice.board[i][j] == "O":
+                    self.lattice.graph_board[i][j][0] = self.o_player.o_image
+                    self.lattice.graph_board[i][j][1] = self.o_player.o_image.get_rect(center=(j * 300 + 150, i * 300 + 150))
+
     def run(self):
+
         running = True
+
         while running:
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         running = False
-                    # раскладка клавы
-                    elif event.key == K_KP1:
-                        self.x_player = X_player(self.screen, 0, 520)
-                        self.x_player.draw_x()
-                        pygame.display.flip()
-                    elif event.key == K_KP2:
-                        self.x_player = X_player(self.screen, 205, 520)
-                        self.x_player.draw_x()
-                        pygame.display.flip()
-                    elif event.key == K_KP1:
-                        self.x_player = X_player(self.screen, 0, 315)
-                        self.x_player.draw_x()
-                        pygame.display.flip()
-                    elif event.key == K_KP1:
-                        self.x_player = X_player(self.screen, 0, 315)
-                        self.x_player.draw_x()
-                        pygame.display.flip()
+
+
+
+
                 elif event.type == QUIT:
                     running = False
 
