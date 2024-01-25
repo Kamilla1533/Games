db84549aa414dc1afb856478534bdf2161dba5a7
Kamilla <155493339+Kamilla1533@users.noreply.github.com>
2024-01-25 15:33:57 +0300

    Update Snake.py

--- SnakeGame/Snake.py
+++ SnakeGame/Snake.py
@@ -3,44 +3,41 @@ from pygame.locals import *
 import time
 import random
 
-
 SIZE = 30
 
+SC_WIDTH = 720
+SC_HEIGHT = 720
+
+BLACK = (51, 44, 57)
+GREEN = (96, 158, 162)
+RED = (201, 44, 109)
+WHITE = (240, 238, 237)
+
 class Apple:
-    def __init__(self, parent_screen, red_color, pos_x, pos_y):
-        self.width = pos_x
-        self.height = pos_y
-        self.sizeApple = 30
-        self.red = red_color
+    def __init__(self, parent_screen):
         self.parent_screen = parent_screen
-        self.apple_pos_x = random.randrange(0, pos_x - self.sizeApple, self.sizeApple)
-        self.apple_pos_y = random.randrange(0, pos_y - self.sizeApple, self.sizeApple)
+        self.apple_pos_x = random.randrange(0, SC_WIDTH - SIZE, SIZE)
+        self.apple_pos_y = random.randrange(0, SC_HEIGHT - SIZE, SIZE)
 
     def draw_random_apple(self):
-        pygame.draw.rect(self.parent_screen, self.red,(self.apple_pos_x, self.apple_pos_y, self.sizeApple, self.sizeApple))
+        pygame.draw.rect(self.parent_screen, RED,(self.apple_pos_x, self.apple_pos_y, SIZE, SIZE))
 
     def another_spawn(self):
-        self.apple_pos_x = random.randrange(0, self.width - self.sizeApple, self.sizeApple)
-        self.apple_pos_y = random.randrange(0, self.height - self.sizeApple, self.sizeApple)
+        self.apple_pos_x = random.randrange(0, SC_WIDTH - SIZE, SIZE)
+        self.apple_pos_y = random.randrange(0, SC_HEIGHT - SIZE, SIZE)
 
 class Snake:
-    def __init__(self, parent_screen, black_color, green_color, pos_x, pos_y, white_color, length):
+    def __init__(self, parent_screen,length):
         self.length = length
-        self.width = pos_x
-        self.height = pos_y
-        self.black = black_color
-        self.green = green_color
-        self.white = white_color
         self.parent_screen = parent_screen
-        self.snake_size = 30
         self.snake_pos_x = [SIZE] * length
         self.snake_pos_y = [SIZE] * length
         self.direction = None
 
     def draw(self):
         for i in range(self.length):
-            pygame.draw.rect(self.parent_screen, self.black,(self.snake_pos_x[i] - 1, self.snake_pos_y[i] - 1, self.snake_size + 2, self.snake_size + 2))
-            pygame.draw.rect(self.parent_screen, self.green,(self.snake_pos_x[i], self.snake_pos_y[i], self.snake_size, self.snake_size))
+            pygame.draw.rect(self.parent_screen, BLACK,(self.snake_pos_x[i] - 1, self.snake_pos_y[i] - 1, SIZE + 2, SIZE + 2))
+            pygame.draw.rect(self.parent_screen, GREEN,(self.snake_pos_x[i], self.snake_pos_y[i], SIZE, SIZE))
 
     def left_move(self):
         self.direction = "left"
@@ -60,26 +57,26 @@ class Snake:
             self.snake_pos_y[i] = self.snake_pos_y[i - 1]
 
         if self.direction == "left":
-            self.snake_pos_x[0] -= self.snake_size
-            if self.snake_pos_x[0] < -self.snake_size:
-                self.snake_pos_x[0] = self.width
+            self.snake_pos_x[0] -= SIZE
+            if self.snake_pos_x[0] < 0:
+                self.snake_pos_x[0] = SC_WIDTH - SIZE
 
         elif self.direction == "right":
-            self.snake_pos_x[0] += self.snake_size
-            if self.snake_pos_x[0] > self.width:
+            self.snake_pos_x[0] += SIZE
+            if self.snake_pos_x[0] == SC_WIDTH:
                 self.snake_pos_x[0] = 0
 
         elif self.direction == "up":
-            self.snake_pos_y[0] -= self.snake_size
-            if self.snake_pos_y[0] < -self.snake_size:
-                self.snake_pos_y[0] = self.height
+            self.snake_pos_y[0] -= SIZE
+            if self.snake_pos_y[0] < 0:
+                self.snake_pos_y[0] = SC_HEIGHT - SIZE
 
         elif self.direction == "down":
-            self.snake_pos_y[0] += self.snake_size
-            if self.snake_pos_y[0] > self.height:
+            self.snake_pos_y[0] += SIZE
+            if self.snake_pos_y[0] == SC_HEIGHT:
                 self.snake_pos_y[0] = 0
 
-        self.parent_screen.fill(self.black)
+        self.parent_screen.fill(BLACK)
         self.draw()
 
     def add_length(self):
@@ -93,29 +90,20 @@ class MainGame:
         pygame.init()
         pygame.mixer.init()
         self.main_song()
-        # размер окна
-        self.SC_WIDTH = 720
-        self.SC_HEIGHT = 720
 
-        # Цвета
-        self.black = (51, 44, 57)
-        self.green = (96, 158, 162)
-        self.red = (201, 44, 109)
-        self.white = (240, 238, 237)
-
-        self.screen = pygame.display.set_mode((self.SC_WIDTH, self.SC_HEIGHT))
+        self.screen = pygame.display.set_mode((SC_WIDTH, SC_HEIGHT))
         title = pygame.display.set_caption("Snake")
-        self.screen.fill(self.black)
+        self.screen.fill(BLACK)
 
-        self.snake = Snake(self.screen, self.black, self.green, self.SC_WIDTH, self.SC_HEIGHT, self.white, 1)
+        self.snake = Snake(self.screen, 1)
         self.snake.draw()
 
-        self.apple = Apple(self.screen, self.red, self.SC_WIDTH, self.SC_HEIGHT)
+        self.apple = Apple(self.screen)
         self.apple.draw_random_apple()
 
     def reset_score(self):
-        self.snake = Snake(self.screen, self.black, self.green, self.SC_WIDTH, self.SC_HEIGHT, self.white, 1)
-        self.apple = Apple(self.screen, self.red, self.SC_WIDTH, self.SC_HEIGHT)
+        self.snake = Snake(self.screen,  1)
+        self.apple = Apple(self.screen)
 
     def collision(self, x1, y1, x2, y2):
         if x1 >= x2 and x1 < x2 + SIZE:
@@ -148,17 +136,19 @@ class MainGame:
 
     def display_score(self):
         font = pygame.font.SysFont("robotho", 50)
-        score = font.render(f"Score: {self.snake.length}", True, self.white)
+        score = font.render(f"Score: {self.snake.length}", True, WHITE)
         self.screen.blit(score, (550, 10))
 
     def game_over_menu(self):
-        self.screen.fill(self.black)
+        self.screen.fill(BLACK)
         font = pygame.font.SysFont("robotho", 40)
-        game_over = font.render(f"GAME OVER", True, self.white)
+        game_over = font.render(f"GAME OVER", True, WHITE)
         self.screen.blit(game_over, (250, 300))
-        score_show = font.render(f"Your score is {self.snake.length}", True, self.white)
+
+        score_show = font.render(f"Your score is {self.snake.length}", True, WHITE)
         self.screen.blit(score_show, (235, 350))
-        play_again = font.render("Press Enter to play or Escape to exit", True, self.white)
+
+        play_again = font.render("Press Enter to play or Escape to exit", True, WHITE)
         self.screen.blit(play_again, (105, 400))
         pygame.display.flip()
         pygame.mixer.music.stop()
@@ -194,7 +184,7 @@ class MainGame:
                 stop = True
                 self.reset_score()
 
-            time.sleep(0.09)
+            time.sleep(0.075)
 
 if __name__ == '__main__':
     Game = MainGame()
